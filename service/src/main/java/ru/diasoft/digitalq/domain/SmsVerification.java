db5e5a925349450f520f6f222e9887efdd862def
ekochetkova <ekochetkova@diasoft.ru>
2021-06-22 11:48:13 +0300

    Remark pom

---
+++ service/src/main/java/ru/diasoft/digitalq/domain/SmsVerification.java
@@ -0,0 +1,32 @@
+package ru.diasoft.digitalq.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "sms_verification")
+public class SmsVerification {
+
+    @Column(name = "verificationid")
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sms_verification_verificationid_seq")
+    @SequenceGenerator(name = "sms_verification_verificationid_seq", sequenceName = "sms_verification_verificationid_seq")
+    private Long verificationId;
+
+    @Column(name = "processguid")
+    private String processGuid;
+
+    @Column(name = "phonenumber")
+    private String phoneNumber;
+
+    @Column(name = "secretcode")
+    private String secretCode;
+
+    @Column(name = "status")
+    private String status;
+}
